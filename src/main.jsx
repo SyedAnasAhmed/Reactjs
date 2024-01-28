daca877654bc8eb100054a92df663d68477ed8b8
Syed Anas Ahmed <syedanasahmed412@gmail.com>
2024-01-28 21:55:43 +0500

    Learned functional components

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,85 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+// // // import React from 'react'
+// // // import ReactDOM from 'react-dom/client'
+// // // import App from './App.jsx'
+// // // import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+// // // ReactDOM.createRoot(document.getElementById('root')).render(
+// // //   <React.StrictMode>
+// // //     <App />
+// // //   </React.StrictMode>,
+// // // )
+
+
+
+// // import React, { Fragment } from "react";
+// // import { ReactDOM } from "react-dom/client";
+// // import App from "./App";
+// // // const root = document.getElementById("root");
+// // // const createRoot = ReactDOM.createRoot(root)
+// // // createRoot.render(
+// // //   <Fragment>
+// // //     <h1>Hello World</h1>
+// // //   </Fragment>
+// // // )
+
+
+// // // const root = document.getElementById("root")
+// // // const createRoot = ReactDOM.createRoot(root)
+// // // createRoot.render(<App />)
+
+
+
+// // ReactDOM.createRoot(document.getElementById("root")).render(
+// //   <h2>ab tu bolde hello world</h2>
+// // )
+
+
+
+// // import React from 'react'
+// // import ReactDOM from 'react-dom/client'
+// // import App from './App.jsx'
+// // import './index.css'
+
+// // ReactDOM.createRoot(document.getElementById('root')).render(
+// //   <React.StrictMode>
+// //     <App />
+// //   </React.StrictMode>,
+// // )
+
+// import React from "react";
+// import ReactDom from "react-dom/client"
+// import App from "./App";
+
+// const root = document.getElementById("root")
+// const createRoot = ReactDom.createRoot(root)
+// createRoot.render(<App />)
+
+
+
+
+
+
+import React from "react"
+import Header from "./components/Header.jsx"
+import Footer from "./components/Footer"
+import "./App.css"
+
+function App() {
+
+  const userName = "Jaffar Aman"
+  // const userName = false
+
+  return (
+
+    <>
+
+      <Header/>
+      <Footer/>
+      <h1 className="helloworld"  >hello world {userName} </h1>
+      <h1>hello world</h1>
+    </>
+
+  )
+}
+
+export default App
